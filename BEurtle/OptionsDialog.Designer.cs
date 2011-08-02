405843603d9a93a04c634603537a48ab9981d581
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere>
2011-08-02 20:54:09 +0100

    * Issue 701/f36 (Add an installer so I can install this easily on my other machines) fixed.

--- BEurtle/OptionsDialog.Designer.cs
+++ BEurtle/OptionsDialog.Designer.cs
@@ -214,7 +214,7 @@
             // 
             this.LinkDonate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.LinkDonate.Image = global::BEurtle.Resource1.Paypal;
+            this.LinkDonate.Image = global::nedprod.Resource1.Paypal;
             this.LinkDonate.Location = new System.Drawing.Point(12, 274);
             this.LinkDonate.Name = "LinkDonate";
             this.LinkDonate.Size = new System.Drawing.Size(340, 158);
--- BEurtle/Resource1.Designer.cs
+++ BEurtle/Resource1.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BEurtle {
+namespace nedprod {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace BEurtle {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BEurtle.Resource1", typeof(Resource1).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("nedprod.Resource1", typeof(Resource1).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
