bd892f1aa0e50b7ad56f9746f11328f6f7fe2298
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@spamtrap.com>
2012-02-22 18:48:09 +0000

    Added lots of graphics resources for upcoming HTML editor etc.

--- BEurtle/OptionsDialog.Designer.cs
+++ BEurtle/OptionsDialog.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OptionsDialog));
             this.label1 = new System.Windows.Forms.Label();
             this.Link_nedproductions = new System.Windows.Forms.LinkLabel();
             this.label2 = new System.Windows.Forms.Label();
@@ -46,7 +47,9 @@
             this.ButtonCancel = new System.Windows.Forms.Button();
             this.ButtonReset = new System.Windows.Forms.Button();
             this.LinkDonate = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.OptionsGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -54,24 +57,24 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(201, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(339, 41);
+            this.label1.Size = new System.Drawing.Size(151, 94);
             this.label1.TabIndex = 0;
             this.label1.Text = "BEurtle v1.02";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Link_nedproductions
             // 
             this.Link_nedproductions.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.Link_nedproductions.Location = new System.Drawing.Point(13, 58);
+            this.Link_nedproductions.Location = new System.Drawing.Point(201, 116);
             this.Link_nedproductions.Name = "Link_nedproductions";
-            this.Link_nedproductions.Size = new System.Drawing.Size(339, 13);
+            this.Link_nedproductions.Size = new System.Drawing.Size(151, 29);
             this.Link_nedproductions.TabIndex = 0;
             this.Link_nedproductions.TabStop = true;
             this.Link_nedproductions.Text = "(C) 2011-2012 Niall Douglas, ned Productions Limited";
-            this.Link_nedproductions.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Link_nedproductions.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.Link_nedproductions.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Link_nedproductions_LinkClicked);
             // 
             // label2
@@ -93,7 +96,7 @@
             this.OptionsGroupBox.Controls.Add(this.CheckDumpHTML);
             this.OptionsGroupBox.Controls.Add(this.BoxBEPath);
             this.OptionsGroupBox.Controls.Add(this.label2);
-            this.OptionsGroupBox.Location = new System.Drawing.Point(13, 75);
+            this.OptionsGroupBox.Location = new System.Drawing.Point(13, 148);
             this.OptionsGroupBox.Name = "OptionsGroupBox";
             this.OptionsGroupBox.Size = new System.Drawing.Size(339, 142);
             this.OptionsGroupBox.TabIndex = 3;
@@ -153,7 +156,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 229);
+            this.label3.Location = new System.Drawing.Point(13, 302);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(73, 13);
             this.label3.TabIndex = 4;
@@ -162,7 +165,7 @@
             // LinkGithubIssues
             // 
             this.LinkGithubIssues.AutoSize = true;
-            this.LinkGithubIssues.Location = new System.Drawing.Point(95, 229);
+            this.LinkGithubIssues.Location = new System.Drawing.Point(95, 302);
             this.LinkGithubIssues.Name = "LinkGithubIssues";
             this.LinkGithubIssues.Size = new System.Drawing.Size(159, 13);
             this.LinkGithubIssues.TabIndex = 6;
@@ -173,7 +176,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 246);
+            this.label4.Location = new System.Drawing.Point(13, 319);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(62, 13);
             this.label4.TabIndex = 6;
@@ -182,7 +185,7 @@
             // LinkHomepage
             // 
             this.LinkHomepage.AutoSize = true;
-            this.LinkHomepage.Location = new System.Drawing.Point(95, 246);
+            this.LinkHomepage.Location = new System.Drawing.Point(95, 319);
             this.LinkHomepage.Name = "LinkHomepage";
             this.LinkHomepage.Size = new System.Drawing.Size(255, 13);
             this.LinkHomepage.TabIndex = 7;
@@ -193,7 +196,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(13, 280);
+            this.label5.Location = new System.Drawing.Point(13, 353);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(281, 13);
             this.label5.TabIndex = 8;
@@ -203,7 +206,7 @@
             // 
             this.ButtonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.ButtonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.ButtonOk.Location = new System.Drawing.Point(277, 467);
+            this.ButtonOk.Location = new System.Drawing.Point(277, 527);
             this.ButtonOk.Name = "ButtonOk";
             this.ButtonOk.Size = new System.Drawing.Size(75, 23);
             this.ButtonOk.TabIndex = 11;
@@ -214,7 +217,7 @@
             // 
             this.ButtonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.ButtonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.ButtonCancel.Location = new System.Drawing.Point(196, 467);
+            this.ButtonCancel.Location = new System.Drawing.Point(196, 527);
             this.ButtonCancel.Name = "ButtonCancel";
             this.ButtonCancel.Size = new System.Drawing.Size(75, 23);
             this.ButtonCancel.TabIndex = 10;
@@ -224,7 +227,7 @@
             // ButtonReset
             // 
             this.ButtonReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.ButtonReset.Location = new System.Drawing.Point(115, 467);
+            this.ButtonReset.Location = new System.Drawing.Point(115, 527);
             this.ButtonReset.Name = "ButtonReset";
             this.ButtonReset.Size = new System.Drawing.Size(75, 23);
             this.ButtonReset.TabIndex = 9;
@@ -237,7 +240,7 @@
             this.LinkDonate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.LinkDonate.Image = global::nedprod.Resource1.Paypal;
-            this.LinkDonate.Location = new System.Drawing.Point(12, 293);
+            this.LinkDonate.Location = new System.Drawing.Point(12, 366);
             this.LinkDonate.Name = "LinkDonate";
             this.LinkDonate.Size = new System.Drawing.Size(340, 158);
             this.LinkDonate.TabIndex = 8;
@@ -246,11 +249,22 @@
             this.LinkDonate.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.LinkDonate.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkDonate_LinkClicked);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::nedprod.Resource1.bugseverwhere;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(182, 132);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
             // OptionsDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(364, 502);
+            this.ClientSize = new System.Drawing.Size(364, 562);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.ButtonReset);
             this.Controls.Add(this.ButtonCancel);
             this.Controls.Add(this.ButtonOk);
@@ -263,14 +277,16 @@
             this.Controls.Add(this.OptionsGroupBox);
             this.Controls.Add(this.Link_nedproductions);
             this.Controls.Add(this.label1);
-            this.MaximumSize = new System.Drawing.Size(32768, 540);
-            this.MinimumSize = new System.Drawing.Size(380, 540);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(32768, 600);
+            this.MinimumSize = new System.Drawing.Size(380, 600);
             this.Name = "OptionsDialog";
             this.Text = "BEurtle Options";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OptionsDialog_FormClosing);
             this.Shown += new System.EventHandler(this.OptionsDialog_Shown);
             this.OptionsGroupBox.ResumeLayout(false);
             this.OptionsGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -296,5 +312,6 @@
         private System.Windows.Forms.Button ButtonReset;
         private System.Windows.Forms.CheckBox CheckAddCommitAsComment;
         private System.Windows.Forms.CheckBox CheckFilterOutClosedIssues;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
