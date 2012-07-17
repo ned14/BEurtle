fa60a2571ff7214f312ff6920bb31dbe3beb0283
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@spamtrap.com>
2012-07-17 19:23:16 +0100

    Added BEgui executable

---
+++ BEgui/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace BEgui
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var dir = args.Length>0 ? args[0] : Directory.GetCurrentDirectory();
+            var plugin = new BEurtle.BEurtlePlugin();
+            var result = plugin.GetCommitMessage((IntPtr)null, "", dir, null, "");
+        }
+    }
+}
